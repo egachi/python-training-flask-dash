6f9705bfabddeaa0b4f93ee2f74c374f045ae1a0
Edison Garcia Chinas <edisga@microsoft.com>
2020-03-31 08:12:19 -0500

    Initial Commit

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ dash = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=app
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
-app.layout = html.Div([
+dash.layout = html.Div([
     dcc.Graph(
         id='life-exp-vs-gdp',
         figure={
