8a7339e7b50a25fd2d605f56f6e2de8b4616e0b0
Edison Garcia Chinas <edisga@microsoft.com>
2020-03-31 08:07:49 -0500

    Initial Commit

--- app.py
+++ app.py
@@ -23,6 +23,7 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 
+import flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -31,8 +32,8 @@ import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+app = flask.Flask(__name__) #Reference https://dash.plotly.com/deployment
+dash = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=app)
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
@@ -67,5 +68,5 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     port= os.environ.get('PORT')
-    app = app.run_server(host='0.0.0.0', debug=False, port=port)
+    app.run(host='0.0.0.0', debug=False, port=port)
     
